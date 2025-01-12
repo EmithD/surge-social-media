7b16bcf51904214e7a272022db43db64cae3b68f
Emith Dinsara <88488807+EmithD@users.noreply.github.com>
2025-01-13 02:55:43 +0530

    init next app and perform express tutorial

--- server/index.js
+++ server/index.js
@@ -3,10 +3,30 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
 import mongoose from 'mongoose';
+import Product from './models/product.model.js';
+import productRoute from './routes/product.route.js';
 // import csurf from 'csurf';
 // import cookieParser from 'cookie-parser';
 
 dotenv.config();
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+//routes
+app.use('/api/products', productRoute);
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message:"Hello World"
+    });
+});
 
 mongoose.connect("mongodb+srv://admin:zOtsESlmD46zvstW@backenddb.wulqw.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
 .then(() => {
@@ -15,18 +35,8 @@ mongoose.connect("mongodb+srv://admin:zOtsESlmD46zvstW@backenddb.wulqw.mongodb.n
     console.log('Connection failed!')
 });
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
-
 app.listen(3000, () => {
     console.log('Server running on 3000');
 })
 
-app.get('/', (req, res) => {
-    res.json({
-        message:"Hello World"
-    });
-});
 
